8523a57acb7f97db075289f0943a25b8c38725f1
Alessandro <luisialessandro95@gmail.com>
2021-12-13 16:50:12 +0100

    Prova di funzionamento

---
+++ src/components/Title.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2>Ciao a tutti quanti</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        nome: 'Title'
+    }
+</script>
+
+<style scoped>
+    h2{
+        color: royalblue;
+    }
+</style>
